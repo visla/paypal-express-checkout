4d8b004cced3723254c91cde3c0315057be287ed
Ian Graham <imartingraham@gmail.com>
2015-07-31 11:14:10 -0700

    Added helper methods to allow for setting the payment action, sub total, tax amount, shipping amount and max amount

--- test/paypal-express-checkout.js
+++ test/paypal-express-checkout.js
@@ -142,4 +142,63 @@ describe('Test paypal-express-checkout', function() {
 			done();
 		});
 	});
+
+	/**
+	 * Additional valid pay calls.
+	 * @param  {[type]} done) {	}         [description]
+	 * @return {[type]}       [description]
+	 */
+	it('should test valid pay call setting amount,payment action, subtotal, max amount plus product info', function(done) {
+		fakeRequest.end = function(params) {
+			assert.ok(params.indexOf('PAYMENTREQUEST_0_AMT=1000.99') !== -1);
+			assert.ok(params.indexOf('EMAIL=test%40email.com') !== -1);
+			assert.ok(params.indexOf('HDRIMG=http%3A%2F%2Fhdrimage.jpg') !== -1);
+			assert.ok(params.indexOf('LOGOIMG=http%3A%2F%2Flogoimg.jpg') !== -1);
+			assert.ok(params.indexOf('PAYFLOWCOLOR=ff00ff') !== -1);
+			assert.ok(params.indexOf('CARTBORDERCOLOR=ffffff') !== -1);
+			assert.ok(params.indexOf('REQCONFIRMSHIPPING=1') !== -1);
+			assert.ok(params.indexOf('NOSHIPPING=0') !== -1);
+			assert.ok(params.indexOf('ALLOWNOTE=1') !== -1);
+			assert.ok(params.indexOf('SOLUTIONTYPE=Sole') !== -1);
+			assert.ok(params.indexOf('L_PAYMENTREQUEST_0_NAME0=product1') !== -1);
+			assert.ok(params.indexOf('L_PAYMENTREQUEST_0_NAME1=product2') !== -1);
+			assert.ok(params.indexOf('L_PAYMENTREQUEST_0_AMT0=10.99') !== -1);
+			assert.ok(params.indexOf('L_PAYMENTREQUEST_0_QTY0=10') !== -1);
+			assert.ok(params.indexOf('L_PAYMENTREQUEST_0_DESC0=description1') !== -1);
+			assert.ok(params.indexOf('RETURNURL=returnUrl') !== -1);
+			assert.ok(params.indexOf('CANCELURL=cancelUrl') !== -1);
+			assert.ok(params.indexOf('PAYMENTREQUEST_0_PAYMENTACTION=Authorization') !== -1);
+			assert.ok(params.indexOf('PAYMENTREQUEST_0_SHIPPINGAMT=12.99') !== -1);
+			assert.ok(params.indexOf('PAYMENTREQUEST_0_TAXAMT=1.49') !== -1);
+			assert.ok(params.indexOf('MAXAMT=1200.99') !== -1);
+		};
+
+		var paypal = Paypal.create('username', 'pswd', 'signature');
+		paypal.setPayOptions('MyBrand', 'http://hdrimage.jpg', 'http://logoimg.jpg', 'ff00ff', 'ffffff', true, false, true);
+		paypal.setProducts([ 
+			{
+				name: 'product1', 
+				description: 'description1', 
+				quantity: 10, 
+				amount: 10.99
+			},
+			{
+				name: 'product2', 
+				description: 'description2', 
+				quantity: 10, 
+				amount: 1000.99
+			},
+			]);
+		paypal.setPaymentAction('Authorization');
+		paypal.setShippingAmount(12.99);
+		paypal.setSubTotal(1000.99);
+		paypal.setTaxAmount(1.49);
+		paypal.setMaxAmount(1200.99);
+
+		paypal.setExpressCheckoutPayment('test@email.com', '001', 1000.99, 'Some description', 'USD', 'returnUrl', 'cancelUrl', false, function(err, data) {
+			console.log('Redirect to paypal: ' + data.redirectUrl);
+			assert.ok(data.redirectUrl.indexOf('new_token') !== -1);
+			done();
+		});
+	});
 });
